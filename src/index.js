2a0415f34e471ab469944252f360b053ea4c5beb
Николай Гребенюк <Yanukhous@ukr.net>
2021-09-27 18:41:09 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const profile = (
+  <div class="profile">
+    <div class="description">
+      <img
+        src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
+        alt="Аватар пользователя"
+        class="avatar"
+      />
+      <p class="name">Petra Marica</p>
+      <p class="tag">@pmarica</p>
+      <p class="location">Salvador, Brasil</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">1000</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">2000</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">3000</span>
+      </li>
+    </ul>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(profile, document.querySelector('#root'));
